558953c600bc6be0d19447139c0b4c9b9b50c2f9
Kesua <papousekjn@gmail.com>
2018-12-15 18:19:25 +0100

    Update Quadtree.py

--- Quadtree.py
+++ Quadtree.py
@@ -1,73 +1,101 @@
-from turtle import speed, exitonclick, hideturtle, penup, setposition, dot, pendown, goto, setup, Screen, Turtle, tracer, update
+from turtle import speed, exitonclick, hideturtle, penup, setposition, dot, pendown, forward, left, goto, setup,setworldcoordinates, Turtle, tracer, update
+from operator import itemgetter
 import json
-X = []
-Y = []
-features = []
-scale_X = []
-scale_Y = []
-
-
-def nahrani_dat():
-
-    with open('export.geojson') as f:
-        data = json.load(f)
-    for feature in data ['features']:
-        X.append(feature['geometry']['coordinates'][0])
-        Y.append(feature['geometry']['coordinates'][1])
-        features.append(feature['geometry']['coordinates'])
-
-    return (X, Y, features)
-
-
-
-
-
-def oblast():
-    UL_point = [min(X),max(Y)]
-    UR_point = [max(X),max(Y)]
-    LL_point = [min(X),min(Y)]
-    LR_point = [max(X),min(Y)]
-    return (UL_point, UR_point, LL_point, LR_point)
-
-
-
-def preskalovani_X():
-    stred_X = (max(X) + min(X)) / 2
-
-    for a in range(len(X)):
-        b = (X[a] - stred_X) * 10000
-        scale_X.append(b)
-    return(scale_X, stred_X)
-
-
-def preskalovani_Y():
-    stred_Y = (max(Y) + min(Y)) / 2
-    for a in range(len(Y)):
-        b = (Y[a] - stred_Y) * 10000
-        scale_Y.append(b)
-    return(scale_Y, stred_Y)
 
 
 def ukaz_data():
+    setworldcoordinates(min(X), min(Y), max(X), max(Y))
     penup()
     hideturtle()
-    tracer(0,0)
-    for a in range(len(scale_X)):
-        goto(scale_X[a],scale_Y[a])
+
+    tracer(0, 0)
+    for a in range(len(X)):
+        goto(X[a], Y[a])
         dot()
     update()
-nahrani_dat()
-oblast()
-preskalovani_X()
-preskalovani_Y()
 
-print(X)
-print(Y)
-print(scale_X)
-print(scale_Y)
 
+def vykresli_oblast(bound):
+    tracer(0,0)
+    hideturtle()
+    penup()
+    goto(bound[0], bound[1])
+    pendown()
+    forward(abs(bound[2]-bound[0]))
+    left(90)
+    forward(abs(bound[3]-bound[1]))
+    left(90)
+    forward(abs(bound[2]-bound[0]))
+    left(90)
+    forward(abs(bound[3]-bound[1]))
+    left(90)
+    penup()
+    update()
+
+
+def body_v_oblasti(body, bound, oblast):
+    B = []
+    for a in range(len(body)):
+
+        ID = body[a][0]
+        x = body[a][1]
+        y = body[a][2]
+        ex_kod = body[a][3]
+        novy_kod = ex_kod + str(oblast)
+        if bound[0] <= x <= bound[2] and bound[1] <= y <= bound[3]:
+            B.append([ID, x, y, novy_kod])
+        else:
+            continue
+    return B
+
+def quadtree(body_v, body_mimo, bound, quad=0, velikost = 10):
+    if len(body_v) <= velikost:  # ukonceni rekurze
+        vykresli_oblast(bound)
+        for point in body_v:
+            body_mimo.append(point)  # vystupni seznam bodu
+        return
+    else:
+        stred = [(bound[0] + bound[2]) / 2, (bound[1] + bound[3]) / 2]
+        quad_1 = [stred[0], stred[1], bound[2], bound[3]]
+        quad_2 = [stred[0], bound[1], bound[2], stred[1]]
+        quad_3 = [bound[0], bound[1], stred[0], stred[1]]
+        quad_4 = [bound[0], stred[1], stred[0], bound[3]]
+        B1 = body_v_oblasti(body_v, quad_1, 1)
+        B2 = body_v_oblasti(body_v, quad_2, 2)
+        B3 = body_v_oblasti(body_v, quad_3, 3)
+        B4 = body_v_oblasti(body_v, quad_4, 4)
+        vykresli_oblast(bound)
+        quadtree(B1, body_mimo, quad_1, 1, numclen)  # rekurze fce v danem bounding boxu
+        quadtree(B2, body_mimo, quad_2, 2, numclen)
+        quadtree(B3, body_mimo, quad_3, 3, numclen)
+        quadtree(B4, body_mimo, quad_4, 4, numclen)
+    return body_mimo
+
+
+
+X = []
+Y = []
+body = []
+numclen = int(input("Zadejte pozadovany pocet clenu ve skupine"))
+with open('export.geojson') as f:
+    data = json.load(f)
+for i, feature in enumerate(data['features']):
+    X.append(feature['geometry']['coordinates'][0])
+    Y.append(feature['geometry']['coordinates'][1])
+    features = feature['geometry']['coordinates']
+    body.append([i, features[0], features[1], ""])
+bound=[min(X), max(X), min(Y), max(Y)]
 ukaz_data()
-#ukaz_data()
+body_mimo = []
+body_mimo = quadtree(body, body_mimo, bound, velikost = numclen)
+ID_body_mimo = (sorted(body_mimo,key=itemgetter(0)))
+
+for i, value in enumerate(data['features']):
+    value['properties']['cluster_id'] = ID_body_mimo[3]
+
+
+with open('import.geojson', 'w') as out:  #ulozeni vystupniho souboru
+    json.dump(data, out)
 
 
 exitonclick()
